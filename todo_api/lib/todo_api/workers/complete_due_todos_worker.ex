c9f64cedf18903cf03e1d6e6e5dea2a59b92abb7
Max Ji <max.ji@investorhub.com>
2026-04-21 11:13:28 +1000

    Wire Oban worker to complete due todos

--- todo_api/lib/todo_api/workers/complete_due_todos_worker.ex
+++ todo_api/lib/todo_api/workers/complete_due_todos_worker.ex
@@ -1,8 +1,22 @@
 defmodule TodoApi.Workers.CompleteDueTodosWorker do
   use Oban.Worker, queue: :default
 
+  alias Oban.Job
+  alias TodoApi.Todos
+
   @impl Oban.Worker
-  def perform(_job) do
+  def perform(%Job{args: args}) do
+    now = now_from_args(args)
+    {:ok, _updated_count} = Todos.complete_todos_due_now(now)
     :ok
   end
+
+  defp now_from_args(%{"run_at" => run_at}) when is_binary(run_at) do
+    case DateTime.from_iso8601(run_at) do
+      {:ok, datetime, _offset} -> datetime
+      _ -> DateTime.utc_now()
+    end
+  end
+
+  defp now_from_args(_args), do: DateTime.utc_now()
 end
